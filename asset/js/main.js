2fb1374648b05078dbf02da052ac05d18690d7f4
MicheleFumi <michele.fumi96@gmail.com>
2024-09-30 14:36:12 +0200

    add subtotal var

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,8 +1,14 @@
 /* console.log('ciao'); */
 
 // FASE 1: CREARE I DUE DATI (PROMPT) KM E ETA'
-let kilometer 
-let age
+let kilometer; 
+let age;
+let ticket_price = 0.21;
+let subtotal = kilometer * ticket_price;
 // PROMPTARE LE SUEGUENTI VARIABLI   
 kilometer= prompt('inserisci i chilometri');
 age= prompt('inserisci gli anni');
+//LOGGARE  KILOMETER, AGE E SUBTOTAL, PER VEDERE SE FUNZIONA IL CALCOLO
+console.log(kilometer);
+console.log(age);
+console.log(subtotal);
