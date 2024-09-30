54a3278c467c47aa1b0a0b93b89fc41b46ac21e7
MicheleFumi <michele.fumi96@gmail.com>
2024-09-30 15:50:04 +0200

    fix log error

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,19 +1,25 @@
 /* console.log('ciao'); */
 
-// FASE 1: CREARE I DUE DATI (PROMPT) KM E ETA'
+// FASE 1: CREIAMO I DUE DATI KM E ETA'
 let kilometer; 
 let age;
 let ticket_price = 0.21;
-let subtotal = kilometer * ticket_price;
-// PROMPTARE LE SUEGUENTI VARIABLI   
-/* kilometer= parseInt(prompt('inserisci i chilometri', "1"), 10); */
+
+// PROMPTIAMO LE SUEGUENTI VARIABLI   
 kilometer = Number(prompt('inserisci i chilometri'));
 age= Number(prompt('inserisci gli anni'));
-//LOGGARE  KILOMETER, AGE E SUBTOTAL, PER VEDERE SE FUNZIONA IL CALCOLO
+
+//CALCOLIAMO SUBTOTAL
+let subtotal = kilometer * ticket_price;
+
+//LOGGHIAMO KILOMETER, AGE E SUBTOTAL, 
 console.log(kilometer);
 console.log(age);
 console.log(subtotal);
+
+
 // FASE 2: CREARE LE VARIABILI DI ETA'
+
 
 if (age < 18){
     console.log((subtotal - 20) / subtotal *100);
