e0d778321fd5b5cc0aa078a58a26d2dddc15c254
MicheleFumi <michele.fumi96@gmail.com>
2024-09-30 15:39:39 +0200

    add number

--- asset/js/main.js
+++ asset/js/main.js
@@ -6,15 +6,16 @@ let age;
 let ticket_price = 0.21;
 let subtotal = kilometer * ticket_price;
 // PROMPTARE LE SUEGUENTI VARIABLI   
-kilometer= parseInt(prompt('inserisci i chilometri'));
-age= prompt('inserisci gli anni');
+/* kilometer= parseInt(prompt('inserisci i chilometri', "1"), 10); */
+kilometer = Number(prompt('inserisci i chilometri'));
+age= Number(prompt('inserisci gli anni'));
 //LOGGARE  KILOMETER, AGE E SUBTOTAL, PER VEDERE SE FUNZIONA IL CALCOLO
 console.log(kilometer);
 console.log(age);
 console.log(subtotal);
 // FASE 2: CREARE LE VARIABILI DI ETA'
 
-if (age < '18'){
+if (age < 18){
     console.log((subtotal - 20) / subtotal *100);
     
 }
