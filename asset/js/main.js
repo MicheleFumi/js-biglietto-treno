2f2a06aba6c5ac7b4403c93b5457e6daa452e63b
MicheleFumi <michele.fumi96@gmail.com>
2024-09-30 15:10:06 +0200

    fix parse

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,12 +1,12 @@
 /* console.log('ciao'); */
 
 // FASE 1: CREARE I DUE DATI (PROMPT) KM E ETA'
-let kilometer =parseInt; 
+let kilometer; 
 let age;
 let ticket_price = 0.21;
 let subtotal = kilometer * ticket_price;
 // PROMPTARE LE SUEGUENTI VARIABLI   
-kilometer= prompt('inserisci i chilometri');
+kilometer= parseInt(prompt('inserisci i chilometri'));
 age= prompt('inserisci gli anni');
 //LOGGARE  KILOMETER, AGE E SUBTOTAL, PER VEDERE SE FUNZIONA IL CALCOLO
 console.log(kilometer);
