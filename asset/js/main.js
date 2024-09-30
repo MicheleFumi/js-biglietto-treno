8c932cb9fee3ddd6e04ae3514e5d4456c92c10c3
MicheleFumi <michele.fumi96@gmail.com>
2024-09-30 13:30:12 +0200

    add variables and prompts

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,1 +1,8 @@
 /* console.log('ciao'); */
+
+// FASE 1: CREARE I DUE DATI (PROMPT) KM E ETA'
+let kilometer 
+let age
+// PROMPTARE LE SUEGUENTI VARIABLI   
+kilometer= prompt('inserisci i chilometri');
+age= prompt('inserisci gli anni');
